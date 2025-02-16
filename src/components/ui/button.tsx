83dff7903667d943197dcd355fddce5c10499f5d
Ali Hasan <alihasan.nsu@gmail.com>
2025-02-16 23:59:42 +0600

    new button added

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,8 +10,8 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
-        destructive:
+          "bg-gradiant-to-b from-blue-600 to-blue-700 text-primary-foregroud",
+        destructive: 
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
