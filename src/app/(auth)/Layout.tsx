8de91442d58e75872308cc837ccfc57b2b48d731
Ali Hasan <alihasan.nsu@gmail.com>
2025-02-24 23:57:54 +0600

    layout design added

--- src/app/(auth)/Layout.tsx
+++ src/app/(auth)/Layout.tsx
@@ -1,9 +1,17 @@
-interface AuthLayoutProps{
+interface AuthLayoutProps {
     children: React.ReactNode
 }
-const AuthLayout = ({children}:AuthLayoutProps) =>{
-    return(
-        <div>{children}</div>
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+    return (
+        <main className="bg-neutral-100 min-h-screen">
+            <div className="mx-auto max-w-screen-2xl p-4">
+                <nav className="flex justify-between items-center">
+                    {/* <Image src="/logo.svg"></Image> */}
+
+                </nav>
+            {children}
+            </div>
+        </main>
     )
 }
 export default AuthLayout
